4fb2e42367663e2ef472ef3906caca26c5ca044e
Leonardo-Sangali <leonardo.dalmonte.s@gmail.com>
2021-09-11 20:36:46 -0300

    Projeto finalizado

---
+++ app/src/main/java/com/example/motivation/infra/SecurityPreferences.kt
@@ -0,0 +1,21 @@
+package com.example.motivation.infra
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SecurityPreferences(val context: Context) {
+
+    private val msharedPreferences = context.getSharedPreferences("motivation", Context.MODE_PRIVATE)
+
+
+    fun storeString(key: String, value: String) {
+        msharedPreferences.edit().putString(key, value).apply()
+    }
+
+    fun getString(key: String): String {
+      return  msharedPreferences.getString(key, "") ?: ""
+
+    }
+
+
+}
